c497f7ee85749656ed7aec4b799e592ec921bd14
FreakyCoder <kurayogun@gmail.com>
2023-09-09 18:20:07 +0300

    feat: version 1 with typescript

--- lib/src/ImprovedTextInput.tsx
+++ lib/src/ImprovedTextInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { TextInput } from "react-native";
 
 const ImprovedTextInput = ({ style, placeholderStyle, value, ...rest }) => (
